afe7db793ec1fdb6cdc2042b479fb48cc072f7a2
amitsaha <amitsaha.in@gmail.com>
2013-03-07 09:30:53 +1000

    data_c.rst: listings, mainly skeletons

---
+++ data_python_c/data_c.rst
@@ -0,0 +1,13 @@
+Assignment statements are copy statements. They store the value of
+the variable or the result of the expression on the right hand side 
+in the memory location pointed to by the variable on the left hand 
+side. 
+
+Any changes to the "original" variable is not reflected in any
+other copies of this variable.
+
+this is a no-op statement.
+     This is a valid C expression, but there is no visible
+     effect of this operation. Unlike Python, data not stored in a
+     variable is not stored and hence isn't allocated any memory.
+
---
+++ data_python_c/listings_c/mut_data.c
@@ -0,0 +1,34 @@
+/* Variables are by default mutable.
+ */
+
+# include <stdio.h>
+
+int a = 1;
+
+int main(int argc, char **argv)
+{
+  int b;
+
+  /*Copy the value stored in a to b*/
+  b = a;
+
+  /* A no-op operation*/
+  2;
+
+  /* The & operator expects an 'lvalue' as an operand, and hence
+     the following statement will result in a compilation error. 
+   */
+  /*printf("Address of 2 %p\n", &2);*/
+
+  printf("Address of a: %p, Address of b: %p\n", &a, &b);
+  printf("a = %d b = %d \n",a,b);
+
+  /* Change value stored in the memory location identified as a.
+   */
+  a = 2;
+
+  printf("Address of a: %p, Address of b: %p\n", &a, &b);
+  printf("a = %d b = %d \n",a,b);
+
+  return 0;
+}
